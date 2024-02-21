b468d94d3abca6b1e20f19cb47e07e1889239a8a
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-02-21 17:06:02 +0100

    fix navigation, add say hello

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="w-full text-gray-600 dark:text-gray-200 lg:w-auto lg:pr-4 lg:pt-0">
+    <div class="mt-12 lg:mt-0">
+        <a href="mailto:hello@joeschroecker.com" class="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max">
+            <span class="relative text-sm font-semibold dark:text-white text-primary">Say Hello</span>
+        </a>
+    </div>
     <div class="card">
         <Menubar :model="items">
             <template #item="{ item, props, hasSubmenu }">
@@ -16,6 +22,7 @@
             </template>
         </Menubar>
     </div>
+  </div>
 </template>
 
 <script setup>
--- pages/index.vue
+++ pages/index.vue
@@ -6,21 +6,7 @@ const route = useRoute()
 
 <template>
   <HeroSection/>
-  <Container>
-  <div>
-    <h1 class="text-3xl font-bold ">Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-    <div>
-    <Panel header="Global" class="mb-4">
-    <div>
-      Content
-    </div>
-    <Button rounded label="Submit"/>
-    </Panel>
-    </div>
-  </div>
-  </Container>
+  <Features/>
 </template>
 
 <style>
