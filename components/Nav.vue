64dcb38a28d71cd6161051ba7c8b3d1b8249997a
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-02-21 16:14:18 +0100

    add preset, nav, layout, components, tailwind

---
+++ components/Nav.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="card">
+        <Menubar :model="items">
+            <template #item="{ item, props, hasSubmenu }">
+                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                    <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                        <span :class="item.icon" />
+                        <span class="ml-2">{{ item.label }}</span>
+                    </a>
+                </router-link>
+                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+                    <span :class="item.icon" />
+                    <span class="ml-2">{{ item.label }}</span>
+                    <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down ml-2" />
+                </a>
+            </template>
+        </Menubar>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+    {
+        label: 'Router',
+        icon: 'pi pi-palette',
+        items: [
+            {
+                label: 'Styled',
+                route: '/theming'
+            },
+            {
+                label: 'Unstyled',
+                route: '/unstyled'
+            }
+        ]
+    },
+    {
+        label: 'External',
+        icon: 'pi pi-home',
+        items: [
+            {
+                label: 'Vue.js',
+                url: 'https://vuejs.org/'
+            },
+            {
+                label: 'Vite.js',
+                url: 'https://vuejs.org/'
+            }
+        ]
+    }
+]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
+import Panel from 'primevue/panel';
 const route = useRoute()
 </script>
 
 <template>
+  <HeroSection/>
+  <Container>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
+    <h1 class="text-3xl font-bold ">Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    <div>
+    <Panel header="Global" class="mb-4">
+    <div>
+      Content
+    </div>
+    <Button rounded label="Submit"/>
+    </Panel>
+    </div>
   </div>
+  </Container>
 </template>
+
+<style>
+@import url("~/assets/css/main.css");
+</style>
