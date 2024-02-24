e0ed6218e89716933a4c8d797b3363293aaf040f
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2024-02-24 16:03:52 +0100

    fix hamburger: isDark

--- components/Hamburger.vue
+++ components/Hamburger.vue
@@ -1,12 +1,15 @@
 <template>
-    <div id="hamburger" class="card flex justify-content-center">
-      <Button class="text-black-important dark:text-white" type="button" icon="pi pi-bars" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu" />
-        <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
-    </div>
+  <div id="hamburger" class="card flex justify-content-center">
+    <Button :class="isDark?'dark:text-white':'text-black-important'" :pt="{icon: {class:isDark?'light:text-black dark:text-white':'text-black dark:text-white'}}" type="button" icon="pi pi-bars" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu" />
+    <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useDark } from '@vueuse/core'
+
+const isDark = useDark()
 
 const menu = ref();
 const { items } = useMenuItems();
@@ -18,5 +21,8 @@ const toggle = (event) => {
 <style>
 .text-black-important {
     color: #000 !important;
+}
+.text-white-important {
+    color: #FFF !important;
 }
 </style>
